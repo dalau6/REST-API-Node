480f62a97619145f488b116dcd7eb8a5fb703c97
David <dalau6@gmail.com>
2020-04-01 11:18:56 -0700

    Patch.

--- users/models/users.model.js
+++ users/models/users.model.js
@@ -25,3 +25,18 @@ exports.findById = id => {
     return result;
   });
 };
+
+exports.patchUser = (id, userData) => {
+  return new Promise((resolve, reject) => {
+    User.findById(id, function(err, user) {
+      if (err) reject(err);
+      for (let i in userData) {
+        user[i] = userData[i];
+      }
+      user.save(function(err, updatedUser) {
+        if (err) return reject(err);
+        resolve(updatedUser);
+      });
+    });
+  });
+};
