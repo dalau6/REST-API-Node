0058c0a9d864dfd9c69a37c756fb4f28e0c65c99
David <dalau6@gmail.com>
2020-04-01 11:16:04 -0700

    Implemented get and post.

--- users/models/users.model.js
+++ users/models/users.model.js
@@ -1,9 +1,27 @@
-const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String,
-    permissionLevel: Number
- });
+const mongoose = require('mongoose');
 
- const userModel = mongoose.model('Users', userSchema);
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  permissionLevel: Number
+});
+
+const User = mongoose.model('Users', userSchema);
+
+exports.createUser = userData => {
+  const user = new User(userData);
+  return user.save();
+};
+
+exports.findById = id => {
+  return User.findById(id).then(result => {
+    result = result.toJSON();
+    delete result._id;
+    delete result.__v;
+    return result;
+  });
+};
